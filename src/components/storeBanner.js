6b34acaa16fea5c3beebc20c1ec8848bcdbf6475
BrettBlox <Diamond5.12>
2019-01-31 12:37:35 -0700

    added apostrophe

--- src/components/storeBanner.js
+++ src/components/storeBanner.js
@@ -8,7 +8,7 @@ const storeBanner = () => (
       <h1>CURRENT INVENTORY</h1>
       <hr />
       <p>
-        These are the pieces that I currently have available. If you dont see
+        These are the pieces that I currently have available. If you don't see
         what you want here, head over to the contact page and shoot me a message
         about commissioning a custom piece and I would be happy to make
         something just for you.
