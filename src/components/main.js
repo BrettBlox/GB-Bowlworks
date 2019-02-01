8cefb3eeec98a26b6bee3b9b74e8bb8780bffd08
BrettBlox <Diamond5.12>
2019-01-31 18:31:04 -0700

    changed text on home page main component

--- src/components/main.js
+++ src/components/main.js
@@ -3,13 +3,16 @@ import React from 'react'
 import '../styles/main.css'
 
 const Main = () => (
-  <div className='cover'>
-    <div className='card description'>
+  <div className="cover">
+    <div className="card description">
       <h1>WELCOME TO GB BOWLWORKS</h1>
       <hr />
       <p>
-        A site showcasing unique, handmade wooden bowls made in Oklahoma. The
+        This site showcases unique handmade wooden bowls made in Oklahoma. The
         gallery below has a few of the past and present pieces out of my studio.
+        Current inventory is available for purchase on the Store page.
+        Questions? Free free to send a message on the Contact page. I'm also on
+        Instagram. Thanks for stopping by!
         <br />
         <br />
         -Greg
