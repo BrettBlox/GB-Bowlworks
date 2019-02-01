ece00bad9e26bd3dc42af1fbba4694b03f3f14ba
BrettBlox <Diamond5.12>
2019-02-01 10:44:48 -0700

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-stripe',
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
