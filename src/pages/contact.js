242103f1bab3a9e052289a09341d0c711212671d
BrettBlox <Diamond5.12>
2019-02-03 16:07:15 -0700

    added recaptcha to form to filter spam/bots

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -32,8 +32,8 @@ export default class Contact extends React.Component {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...this.state
-      })
+        ...this.state,
+      }),
     })
       .then(() => navigateTo(form.getAttribute('action')))
       .catch(error => alert(error))
@@ -42,9 +42,9 @@ export default class Contact extends React.Component {
   render() {
     return (
       <>
-        <div className='site'>
+        <div className="site">
           <SEO
-            title='contact'
+            title="contact"
             keywords={[
               `about`,
               `woodworking`,
@@ -60,68 +60,70 @@ export default class Contact extends React.Component {
               `art`,
               `craft`,
               `handmade`,
-              `etsy`
+              `etsy`,
             ]}
           />
           <Header />
-          <div className='contactWrapper'>
-            <div className='contact'>
+          <div className="contactWrapper">
+            <div className="contact">
               <h1>
                 CONTACT
                 <br /> GB BOWLWORKS
               </h1>
               <hr />
               <form
-                name='contact'
-                method='post'
-                action='/success'
+                name="contact"
+                method="post"
+                action="/success"
                 onSubmit={this.handleSubmit}
-                data-netlify='true'
-                data-netlify-honeypot='bot-field'>
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+              >
                 <input
-                  type='hidden'
-                  name='bot-field'
+                  type="hidden"
+                  name="bot-field"
                   onChange={this.handleChange}
                 />
-                <label htmlFor='name'>
+                <label htmlFor="name">
                   Name:
                   <br />
                   <input
-                    type='text'
-                    name='name'
+                    type="text"
+                    name="name"
                     required
                     onChange={this.handleChange}
                   />
                 </label>
                 <br />
-                <label htmlFor='email'>
+                <label htmlFor="email">
                   Email:
                   <br />
                   <input
-                    type='email'
-                    name='email'
+                    type="email"
+                    name="email"
                     onChange={this.handleChange}
-                    pattern='^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$'
+                    pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$"
                     required
                   />
                 </label>
-                <label htmlFor='tel'>
+                <label htmlFor="tel">
                   Phone Number:
                   <br />
-                  <input type='tel' name='phone' onChange={this.handleChange} />
+                  <input type="tel" name="phone" onChange={this.handleChange} />
                 </label>
                 <br />
-                <label htmlFor='message'>
+                <label htmlFor="message">
                   Message:
                   <br />
                   <textarea
-                    name='message'
+                    name="message"
                     onChange={this.handleChange}
                     required
                   />
                 </label>
+                <div data-netlify-recaptcha="true" />
                 <br />
-                <button type='submit'>Send</button>
+                <button type="submit">Send</button>
               </form>
             </div>
           </div>
