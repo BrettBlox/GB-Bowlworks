1355966531b5341491026b21f0d15a67ae6bf6be
BrettBlox <Diamond5.12>
2019-04-15 20:27:09 -0600

    fixed gallery sort

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -101,13 +101,13 @@ class Gallery extends PureComponent {
     let col4 = []
 
     for (let i = 0; i < gallery.length; i++) {
-      if (i % 4 === 0 && col1.length < 13) {
+      if (i % 4 === 0) {
         col1.push(gallery[i])
-      } else if (i % 4 === 1 && col2.length < 13) {
+      } else if (i % 4 === 1) {
         col2.push(gallery[i])
-      } else if (i % 4 === 2 && col3.length < 13) {
+      } else if (i % 4 === 2) {
         col3.push(gallery[i])
-      } else if (i % 4 === 3 && col3.length < 13) {
+      } else if (i % 4 === 3 && col3.length < 15) {
         col4.push(gallery[i])
       }
     }
