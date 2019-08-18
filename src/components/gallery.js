e4678a27f27787ff79ec524f4f520ddcc5461005
BrettBlox <brettbloxom@gmail.com>
2019-08-18 14:23:17 -0600

    added react-masonry-css

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -130,8 +130,8 @@ class Gallery extends PureComponent {
     }
     // col1 = col1.slice(0, col1.length - 3)
     // col2 = col2.slice(0, col2.length - 2)
-    // col3 = col3.slice(0, col3.length - 4)
-    // col4 = col4.slice(0, col4.length - 3)
+    // col3 = col3.slice(0, col3.length - 1)
+    // col4 = col4.slice(0, col4.length - 1)
 
     const sortedGallery = [...col1, ...col2, ...col3, ...col4]
 
