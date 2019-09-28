df5bc2a2013f5323814cb314871e21253f51bdd4
Brett Bloxom <brettbloxom@gmail.com>
2019-09-28 11:17:38 -0600

    cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,3 +2,25 @@ import CMS from 'netlify-cms-app'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 CMS.registerMediaLibrary(cloudinary)
+
+CMS.init({
+  config: {
+    // load_config_file: false,
+    // backend: {
+    //   name: 'github',
+    //   repo: 'your/repo',
+    //   branch: 'master',
+    //   squash_merges: true, // @NOTE Beta feature
+    // },
+    media_library: {
+      name: 'cloudinary',
+      config: {
+        cloud_name: 'dy6lb8vna',
+        api_key: '241846326259277',
+      },
+    },
+    media_folder: 'src/images',
+    public_folder: 'images',
+    collections: [],
+  },
+})
