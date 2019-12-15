e85c5f77e4b7a53c790267de2ce48e369f18d22f
Brett Bloxom <brettbloxom@gmail.com>
2019-12-15 15:54:52 -0700

    gallery logic changes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -181,7 +181,7 @@ export default function Gallery(props) {
         onClose={closeLightbox}
         preventScroll={props.preventScroll}
       />
-      {/* {viewWidth()} */}
+      {viewWidth()}
       <Spinner />
     </>
   )
