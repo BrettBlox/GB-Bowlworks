c45eb9a4d6ccbe964116b710132fca25b0355b5f
Brett Bloxom <brettbloxom@gmail.com>
2020-05-03 17:57:50 -0600

    removed ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,4 @@
-import React from 'react'
-import Layout from './src/components/layout'
+// import React from 'react'
+// import Layout from './src/components/layout'
 
-export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>
+// export const wrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>
